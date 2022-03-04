4f1be8f1abf74a353605c27fdc461720e0672590
Besanh <anhle3532@gmail.com>
2022-03-04 18:01:28 +0700

    phan chia folder code, workspace

---
+++ x2/hello/hello.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	// Dependency
+	"golang.org/x/example/stringutil"
+)
+
+func main() {
+	// Tra ve chuoi dao nguoc
+	fmt.Println(stringutil.ToUpper("Anh Lê lé"))
+}
